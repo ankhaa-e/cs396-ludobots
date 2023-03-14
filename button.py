000f037d1104d1e8fb134077e4599e66cc903fdb
Ankhaa <ankhaaaae@gmail.com>
2023-03-14 17:44:18 -0500

    little changes

--- button.py
+++ button.py
@@ -2,12 +2,8 @@ import os
 from solution import SOLUTION
 import time
 
-for i in range(10):
-    s = SOLUTION(1)
-    s.Write_Files()
-    s.Start_Simulation(True)
-    s.Wait_For_Simulation_To_End()
+s = SOLUTION(1)
+s.Write_Files()
+s.Start_Simulation(True)
+s.Wait_For_Simulation_To_End()
 
-#for _ in range(10):
-#    os.system("py generate.py")
-#    os.system("py simulate.py GUI x")
